b56ada5232e9d7a917919929d89705c19338a4ef
and-is <andispaudel@gmail.com>
2024-06-19 09:27:54 +0545

    made login signup ui

---
+++ frontend/src/components/Authentication/Signup.jsx
@@ -0,0 +1,86 @@
+import { VStack } from "@chakra-ui/layout";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { useState } from "react";
+import { Button } from "@chakra-ui/button";
+
+const Signup = () => {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [pic, setPic] = useState("");
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  const postDetails = (pics) => {};
+
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing="10px" color="white">
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeholder="Enter your name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>{" "}
+      <FormControl id="confirmPassword" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter your password again"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="pic" isRequired>
+        <FormLabel>Upload your picture</FormLabel>
+        <Input
+          type="file"
+          p="1"
+          placeholder="Enter your email"
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
+        />
+      </FormControl>
+      <Button colorScheme="blue" width="100%" onClick={submitHandler}>
+        Sign Up
+      </Button>
+    </VStack>
+  );
+};
+
+export default Signup;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,61 @@
+import "./Homepage.css";
+import Login from "../components/Authentication/Login.jsx";
+import Signup from "../components/Authentication/Signup.jsx";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        alignItems="center"
+        p={1}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="2px"
+      >
+        <Text fontSize="4xl" color="white" fontWeight="600" textAlign="center">
+          ChatApp
+        </Text>
+      </Box>
+      <Box
+        bg="rgb(0,13,48)"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        borderWidth="4px"
+        borderColor="white"
+        color="white"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default Homepage;
